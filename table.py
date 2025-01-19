715fa4ecc1af537b46370f67e76b43b1ecedb931
MisbahAsghar <asgharali675432@gmail.com>
2025-01-19 15:17:23 +0500

    Add files via upload

---
+++ table.py
@@ -0,0 +1,19 @@
+print("TABLE OF 2")
+for i in range(1,11):
+    print("2 x" , i , "=" , i * 2)
+    
+print("TABLE OF 3")
+for i in range(1,11):
+    print(f"3 x {i} = {5 * i}")    
+
+print("TABLE OF 4")
+for i in range(1,11):
+    print(f"4 x {i} = {4 * i}")
+
+print("TABLE OF 5")
+for i in range(1,11):
+    print(f"5 x {i} = {4 * i}")    
+
+print("TABLE OF 6")
+for i in range(1,11):
+    print(f"6 x {i} = {6 * i}")    
